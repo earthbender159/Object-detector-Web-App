d8667b5edf6f2529574eac7088d538a03cd50120
earthbender159 <80174685+earthbender159@users.noreply.github.com>
2022-08-11 16:14:26 -0400

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,44 @@
+img = ""
+
+function setup(){
+    canvas = createCanvas(640, 420); 
+    canvas.center(); 
+}
+
+function preload(){
+    img = loadImage('abc.jpg'); 
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420); 
+
+    fill('#4B371C'); 
+    text("Bed", 45, 270); 
+    noFill(); 
+    stroke('#4B371C'); 
+    rect(30, 250, 560, 170);
+
+    fill('#9A7B47');  
+    text("Plant", 515, 170); 
+    noFill(); 
+    stroke('#9A7B47');  
+    rect(505, 150, 130, 250); 
+
+    fill('#80471C');  
+    text("Storage", 8, 230); 
+    noFill(); 
+    stroke('#80471C');  
+    rect(5, 210, 130, 180); 
+
+    fill('#795C34');  
+    text("Flowers", 8, 150); 
+    noFill(); 
+    stroke('#795C34');  
+    rect(5, 135, 85, 90); 
+
+    fill('#DC9456');  
+    text("Hamper", 352, 345); 
+    noFill(); 
+    stroke('#DC9456');  
+    rect(345, 330, 115, 90); 
+}
